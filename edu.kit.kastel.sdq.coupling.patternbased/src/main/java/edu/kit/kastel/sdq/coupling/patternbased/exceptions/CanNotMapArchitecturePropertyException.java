420b5890eaaae13105e060768624e0a90873e71a
Laura <laura-traub@hotmail,de>
2022-11-10 19:14:02 +0100

    fixed name of root folder

---
+++ edu.kit.kastel.sdq.coupling.patternbased/src/main/java/edu/kit/kastel/sdq/coupling/patternbased/exceptions/CanNotMapArchitecturePropertyException.java
@@ -0,0 +1,6 @@
+package edu.kit.kastel.sdq.coupling.patternbased.exceptions;
+
+// TODO: should be thrown when an architecture property can not be mapped
+public class CanNotMapArchitecturePropertyException extends Exception {
+
+}
